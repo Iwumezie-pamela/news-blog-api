edb59a72fa689e0afda8b520fe18c90c548428bf
Iwumezie Pamela <chiwumezie@gmail.com>
2024-10-09 17:26:41 +0100

    chore:modify model

--- app/models/ILogin.ts
+++ app/models/ILogin.ts
@@ -2,12 +2,3 @@ export type LoginRequest = {
   email: string;
   password: string;
 };
-
-export type ForgotPasswordRequest = {
-  email: string;
-};
-export type ResetPasswordRequest = {
-  token: string;
-  userId: string;
-  password: string;
-};
