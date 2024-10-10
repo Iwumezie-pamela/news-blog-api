a5ebf39938e54b89caa6c1ad3cf87cd374d6eaea
Iwumezie Pamela <chiwumezie@gmail.com>
2024-10-10 16:15:01 +0100

    feat:api to fetch notifications

---
+++ app/api/notification/route.ts
@@ -0,0 +1,38 @@
+import { prisma } from '@/lib/prisma';
+import { verifyToken } from '@/utils/auth';
+
+// Api to fetch notifications
+export async function GET(request: Request) {
+  try {
+    const verificationResult = await verifyToken(request);
+    if (!verificationResult.success) {
+      return new Response(
+        JSON.stringify({ message: verificationResult.errorMessage }),
+        {
+          status: 401,
+        }
+      );
+    }
+
+    const notifications = await prisma.notification.findMany({
+      where: {
+        userId: verificationResult.data?.userId,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+      select: {
+        id: true,
+        message: true,
+        createdAt: true,
+      },
+    });
+
+    return new Response(JSON.stringify(notifications), { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify({ message: 'Internal Server Error' }), {
+      status: 500,
+    });
+  }
+}
