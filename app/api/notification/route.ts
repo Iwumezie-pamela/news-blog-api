cd0812aeb7002d7bab1bf06d25a121edeb9870a0
Iwumezie Pamela <chiwumezie@gmail.com>
2024-10-30 14:49:41 +0100

    feat:modify

--- app/api/notification/route.ts
+++ app/api/notification/route.ts
@@ -36,3 +36,33 @@ export async function GET(request: Request) {
     });
   }
 }
+
+// Api to delete all notifications
+export async function DELETE(request: Request) {
+  try {
+    const verificationResult = await verifyToken(request);
+    if (!verificationResult.success) {
+      return new Response(
+        JSON.stringify({ message: verificationResult.errorMessage }),
+        {
+          status: 401,
+        }
+      );
+    }
+
+    await prisma.notification.deleteMany({
+      where: {
+        userId: verificationResult.data?.userId,
+      },
+    });
+
+    return new Response(JSON.stringify({ message: 'Notifications deleted' }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify({ message: 'Internal Server Error' }), {
+      status: 500,
+    });
+  }
+}
